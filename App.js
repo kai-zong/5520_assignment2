c186cfb1b47cf8fdf1172e68900a65174cde55e0
kai-zong <zong.k@northeastern.edu>
2024-07-23 19:49:04 -0700

    Add necessary modules and add navigation container in app.js

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import createNativeStackNavigator from '@react-navigation/native-stack';
-import createBottomTabNavigator from '@react-navigation/bottom-tabs';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
+import Activities from './screens/Activities';
+import Diet from './screens/Diet';
+import Setting from './screens/Setting';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen name="Activities" component={Activities} />
         <Tab.Screen name="Diet" component={Diet} />
@@ -16,7 +19,7 @@ export default function App() {
       </Tab.Navigator>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </NavigationContainer>
   );
 }
 
