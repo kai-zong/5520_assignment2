8d79249f351dc3df312b9d53f617e7027f25adbd
kai-zong <zong.k@northeastern.edu>
2024-07-26 10:42:44 -0700

    add icons for the header right pressableButton in InfoPage

--- components/PressableButton.js
+++ components/PressableButton.js
@@ -1,14 +1,22 @@
-import { View, Text, Pressable } from 'react-native'
+import { View, Text, Pressable, StyleSheet } from 'react-native'
 import React from 'react'
 
-export default function PressableButton({bgcolor}) {
+export default function PressableButton({bgcolor, children}) {
   return (
     <View>
-      <Pressable style={[{}, {backgroundColor: bgcolor}]}>
-        <View>
-            {children}
-        </View>
+      <Pressable style={[styles.container, {backgroundColor: bgcolor}]}>
+        {children.map((child, index) => <View key={index}>{child}</View>)}
       </Pressable>
     </View>
   )
 }
+
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+})
--- screens/InfoPage.js
+++ screens/InfoPage.js
@@ -1,15 +1,34 @@
-import { View, Text, SafeAreaView, ScrollView } from 'react-native'
+import { View, Text, SafeAreaView, ScrollView} from 'react-native'
 import { useEffect, useState } from 'react'
 import {useRoute} from '@react-navigation/native'
 import {getItems} from '../firebaseSetup/firebaseHelper'
 import ItemList from '../components/ItemList'
+import PressableButton from '../components/PressableButton'
+import { AntDesign } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
+
 
 export default function InfoPage({navigation}) {
     const [Items, setItems] = useState([]);
-    route = useRoute();
+    const route = useRoute();
     useEffect(() => {
       getItems(route.name).then((items) => {
         setItems(items);
+      });
+
+      navigation.setOptions({
+        headerRight: ()=>{
+          return(
+            <View>
+              <PressableButton bgcolor='green'>
+              <AntDesign name="plus" size={24} color="black" />
+              {(route.name === 'Activities') && <FontAwesome5 name="running" size={24} color="black" />}
+              {(route.name === 'Diet') && <FontAwesome5 name="utensils" size={24} color="black" />}
+
+              </PressableButton>
+            </View>
+          )
+        }
       });
     }, [])
   return (
